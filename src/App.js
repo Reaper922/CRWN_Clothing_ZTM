e3d9bd624d5f936dda3423f1126216c7e3a917bf
Dennis Ammen <dennis.ammen@outlook.com>
2023-10-23 23:23:30 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+
+const Shop = () => {
+  return <h1>I am the shop page.</h1>;
+};
 
 const App = () => (
   <Routes>
-    <Route path="/" element={<Home />}></Route>
+    <Route path="/" element={<Navigation />}>
+      <Route index element={<Home />}></Route>
+      <Route path="shop" element={<Shop />}></Route>
+      <Route path="sign-in" element={<SignIn />}></Route>
+    </Route>
   </Routes>
 );
 
