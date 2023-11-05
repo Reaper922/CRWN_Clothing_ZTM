5f1adde967b4ab7a191a1b39f2980cd35fdeeeb5
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-05 16:07:39 +0100

    Added directory routing

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 import {
   BackgroundImage,
   Body,
@@ -5,10 +7,18 @@ import {
 } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => {
+    navigate(route);
+  };
 
   return (
-    <DirectoryItemContainer className="directory-item-container">
+    <DirectoryItemContainer
+      className="directory-item-container"
+      onClick={onNavigateHandler}
+    >
       <BackgroundImage className="background-image" $imageurl={imageUrl} />
       <Body className="directory-item-body">
         <h2>{title}</h2>
