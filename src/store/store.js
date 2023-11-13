3781f56b4b484b835d08d7a70be162d50d1baae0
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-14 00:00:41 +0100

    Moved logger middleware in its own file

--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { loggerMiddeleware } from "./middleware/logger";
 
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddeleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 
 const persistConfig = {
   key: "root",
@@ -26,8 +13,17 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddeleware];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const middleWares = [
+  process.env.NODE_ENV === "development" && loggerMiddeleware,
+].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
