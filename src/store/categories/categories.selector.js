f23b8fef6129c34c07f409c3c24a34cd2bcf9c11
Dennis Ammen <dennis.ammen@outlook.com>
2023-11-14 17:50:14 +0100

    Implement async redux-thunk categories

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -9,10 +9,16 @@ export const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
-    categories.reduce((acc, category) => {
+  (categories) => {
+    return categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {});
+  }
+);
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
 );
